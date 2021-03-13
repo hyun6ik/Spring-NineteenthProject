fe53ce37ea3c6e84848285d3b6d7241184345768
hyun6ik <fkanwm@naver.com>
2021-03-13 23:22:18 +0900

    프로젝트 생성

---
+++ mvc_standard/src/main/java/com/example/mvc_standard/MvcStandardApplication.java
@@ -0,0 +1,13 @@
+package com.example.mvc_standard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvcStandardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvcStandardApplication.class, args);
+	}
+
+}
