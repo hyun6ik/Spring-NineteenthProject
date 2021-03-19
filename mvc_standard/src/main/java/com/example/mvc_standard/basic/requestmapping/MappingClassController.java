c69f09524d32e97acf4ecec2ee04b9035f91dbfe
hyun6ik <fkanwm@naver.com>
2021-03-19 17:35:18 +0900

    요청 매핑 - API 예시

---
+++ mvc_standard/src/main/java/com/example/mvc_standard/basic/requestmapping/MappingClassController.java
@@ -0,0 +1,43 @@
+package com.example.mvc_standard.basic.requestmapping;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/mapping/users")
+public class MappingClassController {
+
+    /**
+     * 회원 관리 API
+     * 회원 목록 조회 : GET /users
+     * 회원 등록 : POST /users
+     * 회원 조회 : GET /users/{userId}
+     * 회원 수정 : PATCH /users/{userId}
+     * 회원 삭제 : DELETE /users{userId}
+     */
+
+    @GetMapping
+    public String users(){
+        return "get Users";
+    }
+
+    @PostMapping
+    public String addUsers(){
+        return "post user";
+    }
+
+    @GetMapping("/{userId}")
+    public String findUser(@PathVariable String userId){
+        return "get userId = " + userId;
+    }
+
+    @PatchMapping("/{userId}")
+    public String UpdateUser(@PathVariable String userId){
+        return "update userId = " + userId;
+    }
+
+    @DeleteMapping("/{userId}")
+    public String DeleteUser(@PathVariable String userId){
+        return "delete userId = " + userId;
+    }
+
+}
